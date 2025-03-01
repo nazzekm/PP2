525d004a10af7ca5f9288a8a5483d4416eb9ba75
nazzekm <moldakhannazerke2006@gmail.com>
2025-03-01 13:01:50 +0600

    changes

--- lab06/builtin-functions/4.py
+++ lab06/builtin-functions/4.py
@@ -1,8 +1,8 @@
 import time
 import math
 
-num = int(input())  # Число, квадратный корень которого нужно найти
-ms = int(input())   # Задержка в миллисекундах
+num = int(input())  
+ms = int(input())   
 
-time.sleep(ms / 1000)  # Задержка в секундах
+time.sleep(ms / 1000)  
 print(f"Square root of {num} after {ms} milliseconds is {math.sqrt(num)}")
